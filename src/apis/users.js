a5efbdfcb04f973bcee38c95ae329ba875ca2fda
girwarkishor <girwar.kishor@gmail.com>
2021-01-28 23:58:12 +0530

    User Model and Validations

---
+++ src/apis/users.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * @description To create a new User Account
+ * @access /users/api/register
+ * @api Public
+ * @type POST
+ */
+
+export default router;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log("Hello world");
+import cors from "cors";
+import consola from "consola";
+import express from "express";
+import mongoose from "mongoose";
+import { json } from "body-parser";
+
+// Import Application Constants
+import { DB, PORT } from "./constants";
+
+// Router exports
+import userApis from "./apis/users";
+
+// Initialize express application
+const app = express();
+
+// Apply Application Middlewares
+app.use(cors());
+app.use(json());
+
+// Inject Sub router and apis
+app.use("/users", userApis);
+
+const main = async () => {
+  try {
+    // Connect with the database
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+    consola.success("DATABASE CONNECTED!");
+    // Start application listening for request on server
+    app.listen(PORT, () => consola.success(`Server Started on Port ${PORT}`));
+  } catch (err) {
+    consola.error(`Unable to start the server ${err.message}`);
+  }
+};
+
+main();
